4cdfdb9cb61c6f9207cb942f8c82ffb1ab0f4d90
Patrick <patrick@pure-voice.net>
2015-02-27 15:29:13 -0800

    updates

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,2 +1,27 @@
-<div id="header">
+<div class="row top-bar">
+    <div class="col-sm-6 vcenter">
+        <div class="vcenter-this"><img src="/assets/img/recqiLogo_120x36.png" class="img-responsive"></div>
+    </div>
+    <div class="col-sm-6 vcenter">
+       <div class="vcenter-this"><div class="pull-right">User Information</div></div>
+    </div>
+</div>
+
+<div class="row search-grey-container">
+
+    <div class="search-box-conatiner">
+        <div>
+            <div class="give-start-text">GIVE YOUR JOURNEYS A FLYING START</div>
+        </div>
+        <div>
+            <ul id="main-nav">
+                <li class="plan"><a href="#"></a></li>
+                <li class="care active"><a href="#"></a></li>
+                <li class="rent"><a href="#"></a></li>
+                <li class="train"><a href="#"></a></li>
+                <li class="join"><a href="#"></a></li>
+                <li class="services"><a href="#"></a></li>
+            </ul>
+        </div>
+    </div>
 </div>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/assets/css/recqi.css">
 
     <!-- CSS Page Level -->
     @yield('page-css')
@@ -13,8 +14,8 @@
 </head>
 <body>
 
-<div id="wrapper">
-    <header>
+<div id="page-wrapper" class="boxed-wrapper">
+    <header class="header-block clearfix">
         @include('includes.header')
     </header>
 
